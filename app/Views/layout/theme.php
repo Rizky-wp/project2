e61f3dd5e5a40383f3f9e55874b6e385f53bcd02
Rizky-wp <widiputrorizky@gmail.com>
2022-01-28 00:05:59 +0700

    layout navbar, footer

---
+++ app/Views/layout/theme.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- style css -->
+    <link href="/css/style.css" rel="stylesheet">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <!-- navbar section -->
+    <nav class="navbar navbar-expand-lg  ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/img/brand.png" alt="Kementrian Agraria dan Tata Ruang / Badan Pertanahan Nasional" height="100">
+
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end fs-4" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Informasi</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a href="" class="nav-link">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar section -->
+
+    <!-- footer section -->
+
+    <footer class="footer border-top fixed-bottom">
+        <div class="container-fluid px-5">
+            <div class="row  py-4 mt-2 justify-content-between align-items-center">
+                <div class="col col-lg-6 col-md-6 col-sm-3 mb-3">
+                    <div class="row">
+                        <span class="fs-2">Pengarsipan Buku Tanah Kantor Pertanahan kota Tasikmalaya</span>
+                    </div>
+                    <div class="row">
+                        <a class="" href="#">
+                            <img src="/img/brand.png" alt="Kementrian Agraria dan Tata Ruang / Badan Pertanahan Nasional" height="100">
+
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div class="col col-lg-3 col-md-4 col-sm-1">
+
+                    <ul class="nav flex-column fs-6 text-break">
+                        <li class="nav-item mb-1"><a class="nav-link p-0 ">Alamat:<br>
+                                Jl. Boulevard No.1, Sukamanah,Kec.Cipedes<br>
+                                Kota Tasikmalaya</a></li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0 ">Jam Kerja:<br>
+                                Senin – Jum’at<br>
+                                08.00 – 16.00</a></li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0 ">Email:<br>
+                                Pengarsipanbukutanahtasikmalaya@yahoo.com</a></li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0 ">Hubungi Kami:<br>
+                                021-29653300</a></li>
+                    </ul>
+                </div>
+
+
+            </div>
+
+            <div class="row justify-content-center my-3">
+                <span class="text-center">Copyright © 2022 Badan Pertanahan Nasional- All right reserved</span>
+            </div>
+        </div>
+
+    </footer>
+
+    <!-- end footer section -->
+
+
+
+
+
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
