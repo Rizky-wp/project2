fb6ae5a734b9df90f34945eb24adfab5aed0193f
YusufRangkuti <rangkuti289@gmail.com>
2022-01-28 19:46:40 +0700

    Responsive Kontak dan Home

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,7 +2,7 @@
 <?= $this->section('content'); ?>
 <section class="content pt-0">
     <div class="judul container p-5 m-2 justify-content-start">
-        <span>Visi dan Misi</span>
+        <span class="visi">Visi dan Misi</span>
     </div>
     <div class="container d-flex justify-content-center mt-2 mb-5">
         <img src="/img/visi-misi.png" class="img-fluid" />
